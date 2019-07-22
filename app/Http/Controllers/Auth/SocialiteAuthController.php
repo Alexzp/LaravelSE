f128d4d7b6ea361ee969925120a61cda0e6fbdf7
Alexandr Bereznay <alexzpcity@gmail.com>
2019-07-22 14:04:42 +0300

    Add logic : user can create account from socialite, add social account to profile, user can login from social accounts

--- app/Http/Controllers/Auth/SocialiteAuthController.php
+++ app/Http/Controllers/Auth/SocialiteAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\User;
 use App\SocialProfile;
 use Socialite;
@@ -26,11 +27,11 @@ class SocialiteAuthController extends Controller
             //dd($socialUserInfo);
             if (Auth::check()) {
                 // The user is logged in. Probably we need to add this profile for him
-                $this->assignSocialProfileToUser($socialUserInfo);
+                return $this->assignSocialProfileToUser($socialUserInfo,$provider);
             }
             else{
                 // It is guest.We neen register or login him
-                $this->loginOrRegisterUserByProfile($socialUserInfo);
+                return $this->loginOrRegisterUserByProfile($socialUserInfo,$provider);
             }
 
             
@@ -39,24 +40,58 @@ class SocialiteAuthController extends Controller
         }
     }
 
-    private function loginOrRegisterUserByProfile(SocialProfile $profile)
+    private function loginOrRegisterUserByProfile($socialUserInfo,$provider)
     {
-        // Todo : add create\register logic
-
-        // external example : 
-        // $user = User::firstOrCreate(['email' => $socialUserInfo->getEmail()]);
- 
-        // $socialProfile = $user->socialProfile ?: new SocialLoginProfile;
-        // $providerField = "{$provider}_id";
-        // $socialProfile->{$providerField} = $socialUserInfo->getId();
-        // $user->socialProfile()->save($socialProfile);
-
-        // auth()->login($user);
- 
+        
+        $userProfile = SocialProfile::firstOrNew([
+            'email' => $socialUserInfo->getEmail(),
+            'name' => $socialUserInfo->getName()?:$socialUserInfo->getNickname(),
+            'provider' => $provider,
+            'provider_id' => $socialUserInfo->getId(),
+        ]);
+        
+        if ($userProfile->exists) {
+            // userProfile already exists - login him
+            $user = $userProfile->user;
+            Auth::login($user);
+        } else {
+            // userProfile created from 'new'; does not exist in database.
+            // 1. Create new user
+            $newUser = User::create([
+                'name' => $userProfile->name ,
+                'email' => strtolower($userProfile->email),
+                'password' => Hash::make(substr(md5(rand()), 0, 8))
+            ]);
+            // 2. Create profile
+            $userProfile->user_id = $newUser->id;
+            $userProfile->save();
+            // 3. Assign profile to user
+            // 4. login user
+            Auth::login($newUser);
+            
+        }
+        return redirect('home'); 
     }
 
-    private function assignSocialProfileToUser(SocialProfile $profile)
+    private function assignSocialProfileToUser($socialUserInfo,$provider)
     {
         // Todo
+        $userProfile = SocialProfile::firstOrNew([
+            'user_id' => Auth::user()->id,
+            'email' => $socialUserInfo->getEmail(),
+            'name' => $socialUserInfo->getName()?:$socialUserInfo->getNickname(),
+            'provider' => $provider,
+            'provider_id' => $socialUserInfo->getId(),
+        ]);
+
+        if ($userProfile->exists) {
+            $message = "$provider account alredy exists in your profile";
+        }
+        else{
+            $userProfile->save();
+            $message = "$provider account added to your profile";
+        }
+            
+        return redirect('home')->with('status',$message); 
     }
 }
